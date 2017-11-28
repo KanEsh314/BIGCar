ff63aad8011492db647aaf38591b59f59b2eead8
KanEsh314 <kanesh314@gmail.com>
2017-11-28 16:09:47 +0800

    2nd Commit

--- app/src/main/java/my/com/itrain/bigcar/MainActivity.java
+++ app/src/main/java/my/com/itrain/bigcar/MainActivity.java
@@ -3,48 +3,73 @@ package my.com.itrain.bigcar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.explore:
-                    mTextMessage.setText(R.string.title_explore);
-                    return true;
-                case R.id.saved:
-                    mTextMessage.setText(R.string.title_saved);
-                    return true;
-                case R.id.trips:
-                    mTextMessage.setText(R.string.title_trips);
-                    return true;
-                case  R.id.inbox:
-                    mTextMessage.setText(R.string.title_inbox);
-                    return true;
-                case R.id.profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    public void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new ExploreContentFragment(), "Explore");
+        adapter.addFragment(new SavedContentFragment(), "Saved");
+        adapter.addFragment(new TripsContentFragment(), "Trips");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+
+
+        private final List<Fragment> mfragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager){
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position){
+            return mfragmentList.get(position);
+        }
+
+        @Override
+        public int getCount(){
+            return mfragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mfragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mFragmentTitleList.get(position);
+        }
     }
 
 }
---
+++ app/src/main/java/my/com/itrain/bigcar/TripsContentFragment.java
@@ -0,0 +1,29 @@
+package my.com.itrain.bigcar;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TripsContentFragment extends Fragment {
+
+
+    public TripsContentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trips_content, container, false);
+    }
+
+}
