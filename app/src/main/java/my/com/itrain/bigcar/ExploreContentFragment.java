509b074817bd247dd7762329d413fe742416d557
KanEsh314 <kanesh314@gmail.com>
2017-11-30 21:20:03 +0800

    3rd Commit

--- app/src/main/java/my/com/itrain/bigcar/ExploreContentFragment.java
+++ app/src/main/java/my/com/itrain/bigcar/ExploreContentFragment.java
@@ -1,12 +1,21 @@
 package my.com.itrain.bigcar;
 
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +27,6 @@ public class ExploreContentFragment extends Fragment {
     public ExploreContentFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,5 +43,4 @@ public class ExploreContentFragment extends Fragment {
 
         return view;
     }
-
 }
--- app/src/main/java/my/com/itrain/bigcar/ProfileContentFragment.java
+++ app/src/main/java/my/com/itrain/bigcar/ProfileContentFragment.java
@@ -1,11 +1,14 @@
 package my.com.itrain.bigcar;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -23,7 +26,15 @@ public class ProfileContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_content, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_content, container, false);
+
+        String[] profileItem = {"K","A","N","E","S","H"};
+
+        ListView listView = (ListView) view.findViewById(R.id.list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.suggest_item,profileItem);
+        listView.setAdapter(arrayAdapter);
+
+        return view;
     }
 
 }
